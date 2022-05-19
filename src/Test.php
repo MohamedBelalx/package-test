8b1c434ee813f3062ed72f92f9b1952819490fec
mbe <hero.armna@gmail.com>
2022-05-19 13:17:25 +0200

    update info

--- src/Test.php
+++ src/Test.php
@@ -5,7 +5,7 @@ namespace DatabaseService;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class TestModel extends Model
+class Test extends Model
 {
     use HasFactory;
 
